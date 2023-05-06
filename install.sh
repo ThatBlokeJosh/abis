1e1aac190b97c607b431cef50b8a700fed4ea4e0
Joshua Thomas Remington <106017903+ThatBlokeJosh@users.noreply.github.com>
2023-05-06 20:22:21 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+yay -S firefox neovim kwin-bismuth atlauncher balena-etcher discord btop nitch ranger zsh noto-fonts-emoji ttf-meslo-nerd-font-powerlevel10k alacritty
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
+curl -sS https://starship.rs/install.sh | sh
+
+# eval "$(starship init zsh)"
+
